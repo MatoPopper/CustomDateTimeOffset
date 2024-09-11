54eff0f1e4e970988ab50ed921269587c0d7de31
Martin Popper <martin.popper@cpdsystems.sk>
2024-09-11 12:08:49 +0200

    attribute and extensions added

---
+++ CustomDateTimeOffset/Configuration/CustomDateTimeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using CustomDateTimeOffset.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CustomDateTimeOffset.Configuration
+{
+    public static class CustomDateTimeConfiguration
+    {
+        // Generic method for CustomDateTime entity configuration
+        public static void ConfigureCustomDateTime<TEntity>(ModelBuilder modelBuilder, Expression<Func<TEntity, CustomDateTime>> navigationExpression)
+            where TEntity : class
+        {
+            modelBuilder.Entity<TEntity>().OwnsOne(navigationExpression, builder =>
+            {
+                builder.Property(p => p.DateTime)
+                    .HasColumnName($"{GetPropertyName(navigationExpression)}DateTime")
+                    .HasColumnType("timestamp")
+                    .HasConversion(
+                        v => DateTime.SpecifyKind(v, DateTimeKind.Unspecified),
+                        v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+                builder.Property(p => p.Offset)
+                    .HasColumnName($"{GetPropertyName(navigationExpression)}Offset")
+                    .HasColumnType("smallint");
+            });
+        }
+
+        // Helper for get  name 
+        private static string GetPropertyName<TEntity>(Expression<Func<TEntity, CustomDateTime>> expression)
+        {
+            if (expression.Body is MemberExpression memberExpression)
+            {
+                return memberExpression.Member.Name;
+            }
+
+            throw new InvalidOperationException("Invalid expression for property.");
+        }
+    }
+}
---
+++ CustomDateTimeOffset/Extensions/CustomDateTimeModelBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using CustomDateTimeOffset.Attributes;
+using CustomDateTimeOffset.Configuration;
+using CustomDateTimeOffset.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace CustomDateTimeOffset.Extensions
+{
+    public static class CustomDateTimeModelBuilderExtensions
+    {
+        public static void ApplyCustomDateTimeConfiguration(this ModelBuilder modelBuilder)
+        {
+            var entityTypes = modelBuilder.Model.GetEntityTypes().ToList();
+
+            foreach (var entityType in entityTypes)
+            {
+                var entityClrType = entityType.ClrType;
+
+                foreach (var property in entityClrType.GetProperties())
+                {
+                    var customDateTimeAttribute = property.GetCustomAttribute<CustomDateTimeAttribute>();
+                    if (customDateTimeAttribute != null && property.PropertyType == typeof(CustomDateTime))
+                    {
+                        var parameter = Expression.Parameter(entityClrType, "e");
+                        var propertyExpression = Expression.Property(parameter, property.Name);
+                        var lambda = Expression.Lambda(propertyExpression, parameter);
+                        (typeof(CustomDateTimeConfiguration)
+                            .GetMethod(nameof(CustomDateTimeConfiguration.ConfigureCustomDateTime))
+                            ?.MakeGenericMethod(entityClrType))?.Invoke(null, [modelBuilder, lambda]);
+                    }
+                }
+            }
+        }
+    }
+}
