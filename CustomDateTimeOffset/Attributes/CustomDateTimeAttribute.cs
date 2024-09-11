2b08ef09483ccfc7562f0a43e0e8f3bd363a728a
Martin Popper <martin.popper@cpdsystems.sk>
2024-09-11 12:08:49 +0200

    attribute and extensions added

---
+++ CustomDateTimeOffset/Attributes/CustomDateTimeAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace CustomDateTimeOffset.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class CustomDateTimeAttribute : Attribute
+    { }
+}
